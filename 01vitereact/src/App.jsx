3d5c5b9d74fd313bdc3b958f1c0a849d587afbc8
Amit kumar singh <amitsinghdbg2001@gmail.com>
2023-10-14 23:04:54 +0530

    Day 1(about Create React app )

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,16 @@
+import Chai from "./chai"
+function App() {
+  
+
+  return (
+    
+     <> 
+      { /* //This is called as fragment in react by using this we can return many elements within it   */}
+    <Chai/>
+    <h1>Chai or code </h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, ipsum.</p>
+    </>
+  )
+}
+
+export default App
---
+++ basics/src/App.js
@@ -0,0 +1,11 @@
+import Chai from "./Chai";
+function App() {
+  return (
+    <> 
+    <Chai/> 
+    <h1>Hello World</h1>
+    </>
+  );
+}
+
+export default App;
